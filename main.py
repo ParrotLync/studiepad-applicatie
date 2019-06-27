95f145327f798ed541940f92b472d6a898f89b1b
ParrotLync <43622266+ParrotLync@users.noreply.github.com>
2019-06-27 15:54:24 +0200

    Updated all code.
    Added new login class
    Added course read
    Added student info
    Added general menu choice
    
    New workspace files for Alain branch

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import os
 
 
 class SqliteDBConnection:
-    def __init__(self, type_db):
-        self.connect_to_db = sqlite3.connect(type_db)
+    def __init__(self):
+        self.database_path = os.path.join(os.path.dirname('__file__'), 'spa_data.db')
+        self.connect_to_db = sqlite3.connect(self.database_path)
         self.cursor = self.connect_to_db.cursor()
 
     def execute_query(self, input_query, *arg):
@@ -16,90 +17,109 @@ class SqliteDBConnection:
         self.cursor.execute(modify_value_query, arg)
 
 
-class SPA(SqliteDBConnection):
-    def __init__(self, type_user):
-        self.spa_db_path = os.path.join(os.path.dirname(__file__), 'spa_data.db')
-        super().__init__(self.spa_db_path)
-        self.choice_menu = {1: {1: self.print_available_courses,
-                                0: self.close_spa_program},
-                            2: {0: self.close_spa_program},
-                            3: {0: self.close_spa_program}}
-        self.choice_app_list = []
-        self.user_type = type_user
-        self.done_with_app = False
+class SPA:
+    def __init__(self, database, user_type):
+        self.data = database
+        self.done = False
+        self.user_type = user_type
+        self.choices = {}
+        self.options = {1: {1: self.get_courses,
+                            0: self.quit},
+                        2: {0: self.quit},
+                        3: {0: self.quit}}
 
-    def display_menu(self):
+    def get_choices(self):
         if self.user_type is 1:
-            print("\nKies een van de onderstaande taken die u wilt uitvoeren.\n"
-                  "1. Mogelijke keuzevakken\n"
-                  "0. Uitloggen")
-            self.choice_app_list = [1, 0]
-        if self.user_type is 2:
-            print("\nKies een van de onderstaande taken die u wilt uitvoeren.\n"
-                  "1. niets 1\n"
-                  "0. Uitloggen")
-            self.choice_app_list = [0]
-        if self.user_type is 3:
-            print("\nKies een van de onderstaande taken die u wilt uitvoeren.\n"
-                  "1. niets 2\n"
-                  "0. Uitloggen")
-            self.choice_app_list = [0]
+            self.choices = {1: "Mogelijke keuzevakken",
+                            0: "Uitloggen"}
+        elif self.user_type is 2:
+            self.choices = {0: "Uitloggen"}
+        elif self.user_type is 3:
+            self.choices = {0: "Uitloggen"}
 
-    def app_choice(self):
-        valid_menu_choice = False
-        while not valid_menu_choice:
-            try:
-                self.display_menu()
-                choice_app = int(input("\nWat is uw keuze?: "))
-                if choice_app in self.choice_app_list:
-                    action = self.choice_menu[self.user_type].get(choice_app)
-                    action()
-                    valid_menu_choice = True
-                else:
-                    print("\n{} is geen geldige keuze, probeer het nog eens.".format(choice_app))
-            except ValueError:
-                print("\nDat is geen geldige keuze. Probeer het opnieuw.")
+    def start_option(self):
+        self.get_choices()
+        print("\nKies een van de onderstaande opties.")
+        choice = choice_menu(self.choices)
+        option = self.options[self.user_type].get(choice)
+        option()
 
-    def print_available_courses(self):
-        # TODO: Werkt niet, aanpassen
-        courses_query = "SELECT * FROM COURSES"
-        print(self.cursor.description)
-        print(self.execute_query(courses_query))
+    def get_courses(self):
+        query = "SELECT course_id, course_name FROM COURSES"
+        courses = self.data.execute_query(query)
+        for course in courses:
+            print('{:8}{:35}'.format(course[0], course[1]))
 
-    def close_spa_program(self):
-        print("\nU wordt nu uitgelogd. Wij wensen u een fijne dag!")
-        self.done_with_app = True
+    def quit(self):
+        self.done = True
 
 
-def user_credentials():
-    choices = [1, 2, 3, 0]
-    print("\nKies een van de onderstaande opties.\n"
-          "1. Inloggen student\n"
-          "2. Inloggen SLBer\n"
-          "3. Inloggen examencommissie\n"
-          "0. Applicatie afsluiten")
-    while True:
-        try:
-            choice_user = int(input("\nWat is uw keuze?: "))
-            if choice_user in choices:
-                return choice_user
-            else:
-                print("Dat is geen geldige keuze. Probeer het opnieuw.")
-        except ValueError:
-            print("Dat is geen geldige keuze. Probeer het opnieuw.")
+class Login:
+    def __init__(self, database):
+        self.data = database
+        self.type = None
+        self.student = None
+        self.exit = False
+        self.choose_type()
+        super().__init__()
 
-
-def main():
-    done = False
-    while not done:
-        credentials = user_credentials()
-        if credentials is 0:
-            done = True
+    def choose_type(self):
+        choices = {1: "Student",
+                   2: "SLB'er",
+                   3: "Examencommissie",
+                   0: "Programma afsluiten"}
+        print("\nKies een van de onderstaande opties.")
+        choice = choice_menu(choices)
+        if choice is 1:
+            self.type = choice
+            self.choose_student()
         else:
-            use_apps = SPA(credentials)
-            while not use_apps.done_with_app:
-                use_apps.app_choice()
+            self.type = choice
+
+    def choose_student(self):
+        query = "SELECT surname, last_name FROM STUDENTS"
+        students = self.data.execute_query(query)
+        choices = {}
+        n = 1
+        print("\nWat is je naam?")
+        for student in students:
+            choices.update({n: student[0] + ' ' + student[1]})
+            n += 1
+        choice = choice_menu(choices)
+        self.get_student_info(self.type, choice)
+
+    def get_student_info(self, user_type, student_id):
+        query = "SELECT * FROM STUDENTS WHERE student_id = ?"
+        student = self.data.execute_query(query, student_id)
+        self.student = {'Name': student[1] + ' ' + student[2],
+                        'Year': student[3],
+                        'Period': student[4],
+                        'SLB': student[5]}
+
+
+def choice_menu(choices):
+    valid_choice = False
+    while not valid_choice:
+        for x in choices:
+            print(str(x) + '.', choices.get(x))
+        try:
+            choice = int(input(">> "))
+            if choice in choices:
+                return choice
+            else:
+                print("\nDat is geen geldige keuze. Probeer het opnieuw.")
+        except ValueError:
+            print("\nDat is geen geldige keuze. Probeer het opnieuw.")
 
 
 if __name__ == "__main__":
-    main()
+    data = SqliteDBConnection()
+    done = False
+    while not done:
+        user = Login(data)
+        if user.type is 0:
+            done = True
+        else:
+            app = SPA(data, user.type)
+            while not app.done:
+                app.start_option()
